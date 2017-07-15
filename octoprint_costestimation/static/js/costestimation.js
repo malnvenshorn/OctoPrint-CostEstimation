a8503722dc6dc0374e0184f51b1af2ca52e8fdaa
Sven Lohrmann <sven.lohrmann@gmx.net>
2017-07-15 21:40:28 +0200

    Fixed error in view model

--- octoprint_costestimation/static/js/costestimation.js
+++ octoprint_costestimation/static/js/costestimation.js
@@ -4,42 +4,48 @@
  * Author: Sven Lohrmann
  * License: AGPLv3
  */
+
 $(function() {
     function CostEstimationViewModel(parameters) {
         var self = this;
 
-        var printerState = parameters[0];
-        var settings = parameters[1];
+        self.printerState = parameters[0];
+        self.settings = parameters[1];
 
-        printerState.estimatedCost = ko.pureComputed(function() {
-            if (printerState.filename() == undefined) return "-";
+        self.estimatedCostString = ko.pureComputed(function() {
+            if (self.printerState.filename() == undefined || self.printerState.filament().length == 0) {
+                return "-";
+            }
 
-            var costOfFilament = settings.settings.plugins.costestimation.costOfFilament();
-            var weightOfFilament =  settings.settings.plugins.costestimation.weightOfFilament();
-            var densityOfFilament = settings.settings.plugins.costestimation.densityOfFilament();
-            var powerConsumption = settings.settings.plugins.costestimation.powerConsumption();
-            var costOfElectricity = settings.settings.plugins.costestimation.costOfElectricity();
+            var costOfFilament = self.settings.settings.plugins.costestimation.costOfFilament();
+            var weightOfFilament =  self.settings.settings.plugins.costestimation.weightOfFilament();
+            var densityOfFilament = self.settings.settings.plugins.costestimation.densityOfFilament();
+            var powerConsumption = self.settings.settings.plugins.costestimation.powerConsumption();
+            var costOfElectricity = self.settings.settings.plugins.costestimation.costOfElectricity();
             var costPerWeight = costOfFilament / weightOfFilament;
             var costPerHour = powerConsumption * costOfElectricity;
-            var filamentVolume = printerState.filament()[0].data().volume;      // cm³
-            var estimatedPrintTime = printerState.estimatedPrintTime() / 3600;  // h
-            var estimatedCost = costPerWeight * filamentVolume * densityOfFilament
-                                + costPerHour * estimatedPrintTime;
+            var filamentVolume = self.printerState.filament()[0].data().volume;      // cm³
+            var estimatedPrintTime = self.printerState.estimatedPrintTime() / 3600;  // h
+            var estimatedCost = costPerWeight * filamentVolume * densityOfFilament + costPerHour * estimatedPrintTime;
 
             return "" + estimatedCost.toFixed(2) + "€";
         });
 
-        self.onStartup = function() {
-            var element = $("#state").find(".accordion-inner .progress");
+        self.onBeforeBinding = function() {
+            var element = $("#state").find("hr:nth-of-type(2)");
             if (element.length) {
-                var text = gettext("Cost");
-                element.before(text + ": <strong data-bind='text: estimatedCost'></strong><br>");
+                var name = gettext("Cost");
+                var text = gettext("Estimated cost based on required quantity of filament and print time");
+                element.before("<span title='" + text + "'>" + name + "</span>: "
+                                + "<strong id='costestimation' data-bind='text: estimatedCostString'></strong>"
+                                + "<br>");
             }
         };
     }
 
     OCTOPRINT_VIEWMODELS.push({
         construct: CostEstimationViewModel,
-        dependencies: ["printerStateViewModel", "settingsViewModel"]
+        dependencies: ["printerStateViewModel", "settingsViewModel"],
+        elements: ["#costestimation"]
     });
 });
