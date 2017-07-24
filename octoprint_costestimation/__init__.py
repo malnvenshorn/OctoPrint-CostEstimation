a54f0607be7079921365340e4fb98459f888ef3b
malnvenshorn <malnvenshorn@gmail.com>
2017-07-24 21:12:11 +0200

    Implemented settings migration

--- octoprint_costestimation/__init__.py
+++ octoprint_costestimation/__init__.py
@@ -12,6 +12,8 @@ class CostEstimationPlugin(octoprint.plugin.SettingsPlugin,
                            octoprint.plugin.AssetPlugin,
                            octoprint.plugin.TemplatePlugin):
 
+    # SettingsPlugin
+
     def get_settings_defaults(self):
         return dict(
             lastId=0,
@@ -31,16 +33,40 @@ class CostEstimationPlugin(octoprint.plugin.SettingsPlugin,
             currencyFormat="%v %s"      # %v - value, %s - currency symbol
         )
 
+    def get_settings_version(self):
+        return 2
+
+    def on_settings_migrate(self, target, current=None):
+        if current is None:
+            # migrate old settings
+            settings = ["weightOfFilament", "costOfFilament", "densityOfFilament", "diameterOfFilament"]
+
+            filaments = self._settings.get(["filaments"])
+
+            for prop in settings:
+                value = self._settings.get([prop])
+                if value is not None:
+                    filaments[0][prop.replace("OfFilament", "")] = value
+                    self._settings.set([prop], None)
+
+            self._settings.set(["filaments"], filaments)
+
+    # TemplatePlugin
+
     def get_template_configs(self):
         return [
             dict(type="settings", custom_bindings=True),
             dict(type="generic", template="costestimation_filamentdialog.jinja2", custom_bindings=True)
         ]
 
+    # AssetPlugin
+
     def get_assets(self):
         return dict(
-            js=["js/costestimation.js", "js/ko.observableDictionary.js"]
+            js=["js/costestimation.js"]
         )
+
+    # SoftwareUpdate
 
     def get_update_information(self):
         return dict(
